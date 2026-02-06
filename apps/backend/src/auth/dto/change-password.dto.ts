f4a93ddcfded85646db907adc54f6b2b6e26da09
no-e1 <133690936+no-e1@users.noreply.github.com>
2026-02-06 11:08:21 +0100

    feat: confirmPasswd in register.dto

--- apps/backend/src/auth/dto/change-password.dto.ts
+++ apps/backend/src/auth/dto/change-password.dto.ts
@@ -1,4 +1,5 @@
 import { IsString, IsNotEmpty, Length, Matches } from "class-validator";
+import { Match } from "src/common/decorators/match.decorator";
 
 export class ChangePasswordDto {
     @IsString()
@@ -6,9 +7,17 @@ export class ChangePasswordDto {
     oldPassword: string;
 
     @IsString()
-    @Length(8,100)
+    @Length(8,100, {
+        message: 'Password must have at least 8 characters.'
+    })
     @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-        message: 'Password too weak'
+        message: 'Password must include at least one uppercase letter, one lowercase letter, and at least one number or special character.'
     })
     newPassword: string;
+
+
+    @IsString()
+    @IsNotEmpty()
+    @Match('newPassword', { message: 'New passwords do not match'})
+    newPasswordConfirm: string;
 }
--- apps/backend/src/auth/dto/register-user.dto.ts
+++ apps/backend/src/auth/dto/register-user.dto.ts
@@ -1,4 +1,5 @@
-import { IsString, Length, Matches } from "class-validator";
+import { IsNotEmpty, IsString, Length, Matches } from "class-validator";
+import { Match } from "src/common/decorators/match.decorator";
 
 
 
@@ -9,11 +10,19 @@ export class RegisterUsersDto {
     username: string;
 
     @IsString()
-    @Length(8,100)
-    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-        message: 'Password too weak'
+    @Length(8,100, {
+        message: 'Password must have at least 8 characters.'
+    })
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,{
+        message: 'Password must include a t least one uppercase letter, one lowercase letter, and at least one number or special character.'
     })
     password: string;
+
+
+    @IsString()
+    @IsNotEmpty()
+    @Match('password', { message: 'Passwords do not match'})
+    passwordConfirm: string;
 
     @IsString()
     @Length(0,50)
