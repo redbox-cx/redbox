bc57da75392cfc5dd359f1a2f52d8e0bb1ef5530
no-e1 <133690936+no-e1@users.noreply.github.com>
2026-03-04 22:36:13 +0100

    feat: globalPrefix + versioning

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,16 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
