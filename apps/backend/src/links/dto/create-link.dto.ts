395014a1550ef864298fd1afef6f0e51d73dfe31
no-e1 <133690936+no-e1@users.noreply.github.com>
2026-04-08 23:32:21 +0200

    style(links): changed message in create-links dto

--- apps/backend/src/links/dto/create-link.dto.ts
+++ apps/backend/src/links/dto/create-link.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsUrl } from 'class-validator';
 export class CreateLinkDto {
   @IsUrl(
     { require_protocol: true, },
-    { message: 'The URL must start with http:// or https://', },
+    { message: 'Please provide a valid URL', },
   )
   @IsNotEmpty({message: "URL can't be empty"})
   url: string;
