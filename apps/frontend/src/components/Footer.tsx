e81ca6ceb7b2574251ad98beec2a3f0c9e045fc4
Your Name <99895205+HenryZyy@users.noreply.github.com>
2026-04-22 10:31:14 +0200

    fix(footer): change href to link

--- apps/frontend/src/components/Footer.tsx
+++ apps/frontend/src/components/Footer.tsx
@@ -37,11 +37,11 @@ export function Footer() {
                 <div className="footer-column">
                     <h4>Navigation</h4>
                     <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/blog">Blog</a></li>
-                        <li><a href="/contact">Contact</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">Services</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/blog">Blog</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
                     </ul>
                 </div>
 
